0c68822cc253d9486bb05f4b2ee655fab1928566
Константин Герасимов <qn4ajhxzqa@yandex.ru>
2023-10-25 10:05:24 +0700

    SUP-10 improve sentry

---
+++ lib/sentry.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "sentry-ruby"
+
+require_relative "logging"
+
+# https://docs.sentry.io/platforms/ruby/configuration/options/
+Sentry.init do |config|
+  config.environment = ENV.fetch "RAKE_ENV", "development"
+  config.dsn = ENV["SENTRY_DSN"]
+  config.enabled_environments = %w[production]
+  config.logger = Logging.logger
+  config.traces_sample_rate = 0.2
+end
